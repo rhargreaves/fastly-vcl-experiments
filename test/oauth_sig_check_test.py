a33d6ac1ab4160be009bc3cd5dff66d640a8883d
rhargreaves <robert@roberthargreaves.net>
2017-07-16 18:41:04 +0100

    Add token test

--- test/oauth_sig_check_test.py
+++ test/oauth_sig_check_test.py
@@ -11,9 +11,24 @@ proxies = {
 }
 service_host = os.environ['SERVICE_HOST']
 
-def test_returns_200_when_authentication_passes():
+def test_returns_200_when_authentication_passes_for_key_only():
     oauth = OAuth1('foo', client_secret='foo_secret',
              signature_type=SIGNATURE_TYPE_QUERY)
+    url = 'http://{0}/baz'.format(service_host)
+    response = requests.get(
+            url=url, 
+            auth=oauth,
+            proxies=proxies)
+
+    assert_regex(response.text, 'Authenticated!')
+    assert_equals(response.status_code, 200)
+
+def test_returns_200_when_authentication_passes_for_token():
+    oauth = OAuth1('foo', 
+                   client_secret='foo_secret',
+                   resource_owner_key='bar',
+                   resource_owner_secret='bar_secret',
+                   signature_type=SIGNATURE_TYPE_QUERY)
     url = 'http://{0}/baz'.format(service_host)
     response = requests.get(
             url=url, 
